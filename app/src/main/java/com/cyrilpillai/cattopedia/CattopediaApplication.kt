49f57c1ed5ebee1e993037959bd1371a90336f66
cyrilpillai <cyrilpillai@gmail.com>
2024-04-02 00:54:46 +0530

    Add KSP & hilt dependencies

---
+++ app/src/main/java/com/cyrilpillai/cattopedia/CattopediaApplication.kt
@@ -0,0 +1,8 @@
+package com.cyrilpillai.cattopedia
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CattopediaApplication : Application() {
+}
--- app/src/main/java/com/cyrilpillai/cattopedia/MainActivity.kt
+++ app/src/main/java/com/cyrilpillai/cattopedia/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.cyrilpillai.cattopedia.ui.theme.CattopediaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
